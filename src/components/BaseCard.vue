c1627c188ee708a28b16817eb051bcd414eee8ae
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-11 10:28:04 +0700

    Slot Default

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <header>
-            <slot name="header"></slot>
+            <slot name="header" v-if="$slots.header">
+                <h2>The Default Slot Header</h2>
+            </slot>
         </header>
         <slot></slot>
     </div>
